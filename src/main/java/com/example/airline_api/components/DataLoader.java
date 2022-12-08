9755078f271b66ef0a9a21295b33203eab0c5148
rimmderes <rimmderes@gmail.com>
2022-12-08 17:13:03 +0000

    created passenegr and flight repository

--- src/main/java/com/example/airline_api/components/DataLoader.java
+++ src/main/java/com/example/airline_api/components/DataLoader.java
@@ -1,4 +1,15 @@
 package com.example.airline_api.components;
 
-public class DataLoader {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+
+
+
 }
---
+++ src/main/java/com/example/airline_api/repositories/FlightRepository.java
@@ -0,0 +1,9 @@
+package com.example.airline_api.repositories;
+
+import com.example.airline_api.models.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+
+}
